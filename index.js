fbdd1c4d992ee826c40e86469e3f2e770d65ee4e
Grace Luo <grace.r.luo@gmail.com>
2019-10-10 15:38:49 -0700

    tokimon

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:true
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/tokidex'))
+app.get('/tokidex', (req,res) => {
+  
+})
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
