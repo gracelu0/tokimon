bd9751675309c597fef1c3b897fb6f0e9ee0af86
Grace Luo <grace.r.luo@gmail.com>
2019-10-12 00:40:25 -0700

    css

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ var app = express();
 var bodyParser = require('body-parser')
 
 const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl:false
-})
+var pool = new Pool({
+  user: 'graceluo',
+  password: 'tokicorgi',
+  host: 'localhost',
+  database: 'tokimonDB'
+});
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl:false
+// })
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended:false}))
@@ -18,8 +25,38 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/tokidex'))
 app.get('/add', (req, res) => res.render('pages/add'))
 
+// app.get('/tokimons', (req,res) => {
+//   var getUsersQuery = `SELECT * FROM tokimondata`;
+//   console.log(getUsersQuery);
+//   pool.query(getUsersQuery, (error, result) => {
+//     if (error)
+//       res.end(error);
+//     var results = {'rows': result.rows};
+//     console.log(results);
+//     res.render('pages/tokidex',results);
+//     //send response and object over to users.ejs
+//   });
+// });
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM tokimondata');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 app.post('/add', (req,res) => {
   console.log("ok");
+  const total = parseInt(req.body.electric)+parseInt(req.body.fight)+parseInt(req.body.fire)+parseInt(req.body.fly)+parseInt(req.body.water)+parseInt(req.body.ice);
+  //++++;
+  console.log(total);
+  var value = [req.body.tokimonName,req.body.trainer,req.body.height,req.body.weight,req.body.electric,req.body.fight,req.body.fire,req.body.fly,req.body.water,req.body.ice,req.body.food,parseInt(req.body.electric)+parseInt(req.body.fight)+parseInt(req.body.fire)+parseInt(req.body.fly)+parseInt(req.body.water)+parseInt(req.body.ice)];
   var tokiname = req.body.tokimonName;
   var trainer = req.body.trainer;
   var height = req.body.height;
@@ -31,6 +68,7 @@ app.post('/add', (req,res) => {
   var water = req.body.water;
   var ice = req.body.ice;
   var favFood = req.body.food;
+
   res.send(`tokimon name: ${tokiname}`);
 
   console.log(tokiname);
@@ -45,13 +83,11 @@ app.post('/add', (req,res) => {
   console.log(ice);
   console.log(favFood);
 
-  var sql = `INSERT INTO tokimondata (name,trainer,height,weight,electric,fight,fire,fly,water,ice,food) VALUES (tokiname, trainer, height, weight, electric, fight, fire, fly, water,ice,favFood)`;
+  // const inner_results = await client.query('insert into login (id,username,password) values ($1,$2,$3)',value);
 
-  pool.query(sql, (err,result)=> {
-    if (err) throw err;
-    console.log("1 tokimon added");
-  })
-  pool.end();
+  var sql = 'INSERT INTO tokimondata (name,trainer,height,weight,electric,fight,fire,fly,water,ice,food,total) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)';
+  pool.query(sql, value);
+
 
 })
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
