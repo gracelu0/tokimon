d63b7c01d98c7c531f3399eb3bf69f4b2343dc26
Grace Luo <grace.r.luo@gmail.com>
2019-10-15 20:28:51 -0700

    clean up

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ var bodyParser = require('body-parser')
 
 const { Pool } = require('pg');
 
-//LOCAL
-// var pool = new Pool({
-//   user: 'graceluo',
-//   password: 'tokicorgi',
-//   host: 'localhost',
-//   database: 'tokimonDB'
-// });
 
 //Production
 const pool = new Pool({
@@ -33,18 +26,6 @@ function errorHandler(err,req,res,next){
   res.status(500)
   res.render('error', {error:err})
 }
-// app.get('/tokimons', (req,res) => {
-//   var getUsersQuery = `SELECT * FROM tokimondata`;
-//   console.log(getUsersQuery);
-//   pool.query(getUsersQuery, (error, result) => {
-//     if (error)
-//       res.end(error);
-//     var results = {'rows': result.rows};
-//     console.log(results);
-//     res.render('pages/tokidex',results);
-//     //send response and object over to users.ejs
-//   });
-// });
 
 app.get('/db', async (req, res) => {
     try {
@@ -93,8 +74,6 @@ app.get('/deleteTokimon/:name', async (req, res) => {
       console.log(req.params.name);
       //res.send(`tokimon name: ${req.params.name}`);
       const result = await client.query(`DELETE FROM tokimondata WHERE name = '${req.params.name}'`);
-      //res.send(`deleted: ${req.params.name}`)
-      // const deleted = { 'results': (result) ? result.rows : null};
        res.render('pages/deleteSuccess');
       client.release();
     } catch (err) {
@@ -109,7 +88,6 @@ app.get('/edit/:name', async (req, res) => {
       console.log(req.params.name);
       //res.send(`tokimon name: ${req.params.name}`);
       const result = await client.query(`SELECT * FROM tokimondata WHERE name = '${req.params.name}'`);
-      //res.send(`deleted: ${req.params.name}`)
        const results = { 'results': (result) ? result.rows : null};
        res.render('pages/edit', results);
       client.release();
@@ -122,15 +100,11 @@ app.get('/edit/:name', async (req, res) => {
 app.post('/update/:name', (req,res)=> {
   console.log(req.body);
   var value = [req.body.trainer,req.body.height,req.body.weight,req.body.electric,req.body.fight,req.body.fire,req.body.fly,req.body.water,req.body.ice,req.body.food,parseInt(req.body.electric)+parseInt(req.body.fight)+parseInt(req.body.fire)+parseInt(req.body.fly)+parseInt(req.body.water)+parseInt(req.body.ice)];
-  // var sql = `UPDATE tokimondata SET trainer = req.body.trainer, height = req.body.height, weight = req.body.weight,
-  //   electric = req.body.electric, fight = req.body.fight, fire = req.body.fire, fly = req.body.fly, water = req.body.water, ice = req.body.ice,
-  //   food = req.body.food WHERE name = '${req.params.name}'`;
   var sql = `UPDATE tokimondata SET trainer = $1, height = $2, weight = $3,
     electric = $4, fight = $5, fire = $6, fly = $7, water = $8, ice = $9,
     food = $10, total = $11 WHERE name = '${req.params.name}'`;
 
   pool.query(sql, value, (err, result)=> {
-    //if (err) throw err;
     if (err) res.render('pages/updateError');
     res.render('pages/updateSuccess');
     console.log("tokimon updated!");
